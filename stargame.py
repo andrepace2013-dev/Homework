8bf1ecfad248a172230aab54d9cdffd2b0860461
andrepace2013-dev <andre.pace.2013@gmail.com>
2026-08-24 18:11:44 +0200

    Add files via upload

---
+++ stargame.py
@@ -0,0 +1,50 @@
+import pgzrun
+import time
+import random
+WIDTH = 800
+HEIGHT = 600
+stars = []
+number_of_star = 8
+next_star = 0
+lines = []
+start_time = 0
+total_time = 0
+end_time = 0
+def create_star():
+    global start_time
+    for i in range(number_of_star):
+        star = Actor("star")
+        star.pos = random.randint(50,750),random.randint(50,550)
+        stars.append(star)
+    start_time = time.time()
+create_star()
+def draw():
+    global total_time
+    screen.blit("sky",(0,0))
+    number = 1
+    for star in stars:
+        star.draw()
+        screen.draw.text(str(number),(star.pos[0],star.pos[1]+20))
+        number+=1
+    for line in lines:
+        screen.draw.line(line[0], line[1], (255,255,255))
+    if next_star<number_of_star:
+        total_time = time.time() - start_time
+        screen.draw.text(str(round(total_time,1)),(10,10),fontsize=30)
+    else:
+        screen.draw.text(str(round(total_time,1)),(10,10),fontsize=30)
+    if next_star==number_of_star:
+        screen.draw.text("YOU WIN",(300,300),fontsize=55)
+def update():
+    pass
+def on_mouse_down(pos):
+    global next_star, lines
+    if next_star<number_of_star:
+        if stars[next_star].collidepoint(pos):
+            if next_star:
+                lines.append((stars[next_star-1].pos,stars[next_star].pos))
+            next_star=next_star+1
+        else:
+            lines=[]
+            next_star=0
+pgzrun.go()
